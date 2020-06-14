31f8acc445b0014a20ea1071d4fde2292a9632c1
paulo <paulodlima2014@gmail.com>
2020-06-14 19:46:02 -0300

    ajustado o telefone em perfil do usuario e agora está ocutando as roles em editar usuario

--- src/app/pages/user-profile/user-profile.component.ts
+++ src/app/pages/user-profile/user-profile.component.ts
@@ -20,6 +20,7 @@ export class UserProfileComponent implements OnInit {
   public sucesso = false;
   desabilitado = true;
   private pessoaToken: Pessoa[];
+  telMask = [/\d/, /\d/, /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/, /\d/];
 
   constructor(private formBuilder: FormBuilder,
               private pessoasService: PessoasService,
@@ -31,13 +32,13 @@ export class UserProfileComponent implements OnInit {
       this.authService.logout();
     }
     this.formPerson = this.formBuilder.group({
-      fullName: ['', [Validators.required, Validators.pattern(/^[\s\S]{5,40}$/)]],
-      documentNumber: ['', [Validators.required, Validators.pattern(/^[0-9]{11}$/)]],
+      fullName: ['', [Validators.required, Validators.pattern(/^[a-z A-Z]{5,40}$/)]],
+      documentNumber: ['', [Validators.required]],
       email: ['', [Validators.email, Validators.required]],
       birthDate: ['', Validators.required],
       sexType: ['', Validators.required],
-      fathersName: ['', [Validators.required, Validators.pattern(/^[\s\S]{5,40}$/)]],
-      mothersName: ['', [Validators.required, Validators.pattern(/^[\s\S]{5,40}$/)]],
+      fathersName: ['', [Validators.required, Validators.pattern(/^[a-z A-Z]{5,40}$/)]],
+      mothersName: ['', [Validators.required, Validators.pattern(/^[a-z A-Z]{5,40}$/)]],
       address: this.formBuilder.group({
         zipCode: ['', [Validators.required]],
         city: ['', Validators.required],
@@ -47,14 +48,14 @@ export class UserProfileComponent implements OnInit {
         neighborhood: ['', Validators.required]
       }),
       phone: this.formBuilder.group({
-        areaCode: ['', [Validators.required, Validators.pattern(/^-?(0|[0-9]\d*)?$/)]],
-        number: ['', [Validators.required, Validators.pattern(/^-?(0|[0-9]{8,9}\d*)?$/)]]
+        areaCode: ['', [Validators.required, Validators.pattern(/^-?(0|[0-9]{0,2}\d*)?$/)]],
+        number: ['', [Validators.required]]
       }),
       height: this.formBuilder.group({
-        height: ['', [Validators.required, Validators.pattern(/^\d+(\.\d+)*$/)]],
+        height: ['', [Validators.required]],
       }),
       weight: this.formBuilder.group({
-        weight: ['', [Validators.required, Validators.pattern(/^\d+(\.\d+)*$/)]]
+        weight: ['', [Validators.required]]
       })
     });
 
