0ef2656852470cd255b3423d55af4d8714804bc9
paulo <paulodlima2014@gmail.com>
2020-06-14 13:43:52 -0300

    ajustes de icones

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -15,17 +15,17 @@ export const ROUTES: RouteInfo[] = [
 ];
 
 export const ROUTESADMIN: RouteInfo[] = [
-  { path: '/locais', title: 'Locais',  icon: 'ni-tv-2 text-primary', class: ''},
+  { path: '/locais', title: 'Locais',  icon: 'ni ni-square-pin text-primary', class: ''},
   { path: '/pessoas', title: 'Consulta de Pessoas',  icon: 'fas fa-search', class: '' },
-  { path: '/adicionar-vacina', title: 'Adicionar vacina a calendario',  icon: 'ni-planet text-blue', class: '' },
-  { path: '/user-profile', title: 'Perfil',  icon: 'ni-single-02 text-yellow', class: '' },
-  { path: '/minhas-vacinas', title: 'Minhas vacinas',  icon: 'fas fa-search', class: '' },
-  { path: '/campanha', title: 'Campanha',  icon: 'ni-single-02 text-yellow', class: '' },
+  { path: '/adicionar-vacina', title: 'Adicionar vacina a calendario',  icon: 'ni ni-fat-add text-blue', class: '' },
+  { path: '/user-profile', title: 'Perfil',  icon: 'ni ni-circle-08 text-yellow', class: '' },
+  { path: '/minhas-vacinas', title: 'Minhas vacinas',  icon: 'ni ni-single-copy-04 text-red', class: '' },
+  { path: '/campanha', title: 'Campanha',  icon: 'ni ni-send text-yellow', class: '' },
   { path: '/lista-vacinas', title: 'Lista de vacinas',  icon: 'fas fa-search', class: '' },
   { path: '/add-vacina-pessoa', title: 'Aplicar vacina pessoa',  icon: 'ni-single-02 text-yellow', class: '' },
-  { path: '/cadastrar-vacina', title: 'Cadastrar Nova Vacina',  icon: 'ni-key-25 text-info', class: '' },
+  { path: '/cadastrar-vacina', title: 'Cadastrar Nova Vacina',  icon: 'ni ni-ruler-pencil text-info', class: '' },
   { path: '/cadastrar-pessoa', title: 'Cadastro de pessoa',  icon: 'fas fa-users', class: '' },
-  { path: '/cadastrar-postos', title: 'Cadastro de Postos de vacinação',  icon: 'fas fa-users', class: '' }
+  { path: '/cadastrar-postos', title: 'Cadastro de Postos de vacinação',  icon: 'ni ni-building', class: '' }
 ];
 
 @Component({
